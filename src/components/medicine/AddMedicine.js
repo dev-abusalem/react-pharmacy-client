5a29d327cfd784007b233bacb9a28ab08aee1473
dev.abusalem <dev.abusalem@gmail.com>
2024-01-09 21:40:13 +0600

    update dsfds

--- src/components/medicine/AddMedicine.js
+++ src/components/medicine/AddMedicine.js
@@ -2,134 +2,126 @@ import React, { useEffect, useState } from "react";
 import "./scss/addmedicine.scss";
 import { GoThreeBars } from "react-icons/go";
 import { Link } from "react-router-dom";
-import imgpreview from "../../assets/images/avater.jpg"
+import imgpreview from "../../assets/images/avater.jpg";
 import axios from "axios";
-import {ToastContainer, toast} from "react-toastify"
+import { ToastContainer, toast } from "react-toastify";
 
 function AddMedicine() {
-  const [cate, setCate] = useState([])
-  const [units, setUnit] = useState([])
-  const [types, setType] = useState([])
+  const [cate, setCate] = useState([]);
+  const [units, setUnit] = useState([]);
+  const [types, setType] = useState([]);
 
   // State of change value of select tag
-  const [unitValue, setUnitValue] = useState(null)
-  const [cateValue, setCateValue] = useState(null)
-//fullname,email1,phone,address1,state,country,mobile,email2,contact,address2,city,zip
-const [barcode, setBarcode] = useState("");
-const [strength , setStrength] = useState("");
-const [boxsize, setBoxSize] = useState("");
-const [shelf , setShelf ] = useState("");
-const [mcategory , setmMategory ] = useState("");
-const [medicinetype, setMedicineType] = useState("");
-const [manufacturer , setManufacturer ] = useState("");
-const [vat, setVat] = useState("");
-const [status , setStatus ] = useState("Active");
-const [mname , setMname ] = useState("");
-const [gname , setGname ] = useState("");
-const [munit , setMunit ] = useState("");
-const [medicinedetails , setMedicineDetails ] = useState("");
-const [price  , setPrice ] = useState("");
-const [mphoto  , setMphoto ] = useState("");
-const [manufacprice  , setManufacPrice ] = useState("");
-const [igta   , setIGTA  ] = useState("");
-const [expairDate   , setExpairDate  ] = useState("");
+  const [unitValue, setUnitValue] = useState(null);
+  const [cateValue, setCateValue] = useState(null);
+  //fullname,email1,phone,address1,state,country,mobile,email2,contact,address2,city,zip
+  const [barcode, setBarcode] = useState("");
+  const [strength, setStrength] = useState("");
+  const [boxsize, setBoxSize] = useState("");
+  const [shelf, setShelf] = useState("");
+  const [mcategory, setmMategory] = useState("");
+  const [medicinetype, setMedicineType] = useState("");
+  const [manufacturer, setManufacturer] = useState("");
+  const [vat, setVat] = useState("");
+  const [status, setStatus] = useState("Active");
+  const [mname, setMname] = useState("");
+  const [gname, setGname] = useState("");
+  const [munit, setMunit] = useState("");
+  const [medicinedetails, setMedicineDetails] = useState("");
+  const [price, setPrice] = useState("");
+  const [mphoto, setMphoto] = useState("");
+  const [manufacprice, setManufacPrice] = useState("");
+  const [igta, setIGTA] = useState("");
+  const [expairDate, setExpairDate] = useState("");
 
+  const [previousMphoto, setPreviousMphoto] = useState(null);
+  // image and form submit
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-const [previousMphoto, setPreviousMphoto] = useState(null);
-// image and form submit
-
-
-const handleSubmit = async (e) =>{
-e.preventDefault();
-
-  const formData = new FormData();
-  formData.append('mimage', previousMphoto);
-  const imageUploadResponse = await axios.post("/upload", formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data', // Important for file uploads
-    },
+    const formData = new FormData();
+    formData.append("mimage", previousMphoto);
+    const imageUploadResponse = await axios.post("/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data", // Important for file uploads
+      },
     });
 
     setPreviousMphoto(imageUploadResponse.data);
-    console.log(imageUploadResponse)
+    console.log(imageUploadResponse);
 
-try {
-  const res = await axios.post("/medicine/medicine/add",
-  {
-    barcode:barcode,
-    strength:strength,
-    boxsize:boxsize,
-    shelf:shelf,
-    mcategory:mcategory,
-    medicinetype:medicinetype,
-    manufacturer:manufacturer,
-    expairDate:expairDate,
-    vat:vat,
-    status:status,
-    medicinename:mname,
-    genericname:gname,
-    munit:munit,
-    medicinedetails:medicinedetails,
-    medicineprice:price,
-    mfeatureimage:imageUploadResponse.data,
-    manufactureprice:manufacprice,
-    igta:igta,
-  }
-  );
+    try {
+      const res = await axios.post("/medicine/medicine/add", {
+        barcode: barcode,
+        strength: strength,
+        boxsize: boxsize,
+        shelf: shelf,
+        mcategory: mcategory,
+        medicinetype: medicinetype,
+        manufacturer: manufacturer,
+        expairDate: expairDate,
+        vat: vat,
+        status: status,
+        medicinename: mname,
+        genericname: gname,
+        munit: munit,
+        medicinedetails: medicinedetails,
+        medicineprice: price,
+        mfeatureimage: imageUploadResponse.data,
+        manufactureprice: manufacprice,
+        igta: igta,
+      });
 
-  console.log(imageUploadResponse.data)
-  toast.success(res.data);
-  
-  // setTimeout(()=>{
-  //   window.location.reload(true)
-  // }, 3000)
+      console.log(imageUploadResponse.data);
+      toast.success(res.data);
 
-} catch (error) {
-  toast.error(error.response.data)
-}
-}
+      // setTimeout(()=>{
+      //   window.location.reload(true)
+      // }, 3000)
+    } catch (error) {
+      toast.error(error.response.data);
+    }
+  };
 
-// upload medicine image
+  // upload medicine image
 
-
-  const showAllCate = async () =>{
+  const showAllCate = async () => {
     const res = await axios.get("/medicine/cate");
     setCate(res.data);
-  }
-  useEffect(()=>{  
-    showAllCate()
-  },[])
+  };
+  useEffect(() => {
+    showAllCate();
+  }, []);
 
-  const showAllUnit = async () =>{
+  const showAllUnit = async () => {
     const res = await axios.get("/medicine/unit");
     setUnit(res.data);
-  }
-  useEffect(()=>{  
-    showAllUnit()
-  },[])
+  };
+  useEffect(() => {
+    showAllUnit();
+  }, []);
 
-  const showAllType = async () =>{
+  const showAllType = async () => {
     const res = await axios.get("/medicine/type");
     setType(res.data);
-  }
-  useEffect(()=>{  
-    showAllType()
-  },[])
+  };
+  useEffect(() => {
+    showAllType();
+  }, []);
 
   // Use the uploaded image path in your main request
-const handleImageChange = (e) => {
-  const file = e.target.files[0];
-  setPreviousMphoto(file);
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      setMphoto(reader.result);
-    };
-    reader.readAsDataURL(file);
-  }
-};
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setPreviousMphoto(file);
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setMphoto(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <section>
       <ToastContainer />
@@ -144,7 +136,6 @@ const handleImageChange = (e) => {
               <GoThreeBars />
               <Link to="/medicine/list">Medicine List</Link>
             </div>
-             
           </div>
         </div>
 
@@ -157,14 +148,17 @@ const handleImageChange = (e) => {
                   placeholder="Bar Code/QR Code"
                   type="text"
                   id="barcode"
-                  onChange={(e)=>setBarcode(e.target.value)}
+                  onChange={(e) => setBarcode(e.target.value)}
                 />
               </div>
 
               <div className="form_flied">
                 <label htmlFor="strength">Strength :</label>
-                <input placeholder="Strength" type="text" id="strength"
-                 onChange={(e)=>setStrength(e.target.value)}
+                <input
+                  placeholder="Strength"
+                  type="text"
+                  id="strength"
+                  onChange={(e) => setStrength(e.target.value)}
                 />
               </div>
 
@@ -173,17 +167,21 @@ const handleImageChange = (e) => {
                   {" "}
                   Box Size <span style={{ color: "red" }}>*</span> :
                 </label>
-                <input  placeholder="Box Size" type="text" id="boxsize" 
-                onChange={(e)=>setBoxSize(e.target.value)}
-
+                <input
+                  placeholder="Box Size"
+                  type="text"
+                  id="boxsize"
+                  onChange={(e) => setBoxSize(e.target.value)}
                 />
               </div>
 
               <div className="form_flied">
                 <label htmlFor="shelf"> Shelf :</label>
-                <input placeholder="Shelf" type="text" id="shelf" 
-                                onChange={(e)=>setShelf(e.target.value)}
-
+                <input
+                  placeholder="Shelf"
+                  type="text"
+                  id="shelf"
+                  onChange={(e) => setShelf(e.target.value)}
                 />
               </div>
 
@@ -192,29 +190,46 @@ const handleImageChange = (e) => {
                   {" "}
                   Medicine Category <span style={{ color: "red" }}>*</span> :
                 </label>
-                <select onChange={(e)=>setmMategory(e.target.value)} id="medicinecate">
-                <option>Select An Option</option>
+                <select
+                  onChange={(e) => setmMategory(e.target.value)}
+                  id="medicinecate"
+                >
+                  <option>Select An Option</option>
 
-                  {
-                    cate?.map((cat,i)=>{
-                        return <option value={cat.catename} key={i}>{cat.catename}</option>
+                  {cate ? (
+                    cate.map((cat, i) => {
+                      return (
+                        <option value={cat.catename} key={i}>
+                          {cat.catename}
+                        </option>
+                      );
                     })
-                  }
-                </select> 
-                
+                  ) : (
+                    <option>Loading......</option>
+                  )}
+                </select>
               </div>
-              
+
               <div className="form_flied">
                 <label htmlFor="medicinetype"> Medicine Type :</label>
-                <select onChange={(e)=>setMedicineType(e.target.value)} id="medicinecate">
-                <option>Select An Option</option>
+                <select
+                  onChange={(e) => setMedicineType(e.target.value)}
+                  id="medicinecate"
+                >
+                  <option>Select An Option</option>
 
-                  {
-                    types?.map((type,i)=>{
-                        return <option value={type.typename} key={i}>{type.typename}</option>
+                  {types ? (
+                    types.map((type, i) => {
+                      return (
+                        <option value={type.typename} key={i}>
+                          {type.typename}
+                        </option>
+                      );
                     })
-                  }
-                </select> 
+                  ) : (
+                    <option>Loading......</option>
+                  )}
+                </select>
               </div>
 
               <div className="form_flied">
@@ -226,8 +241,7 @@ const handleImageChange = (e) => {
                   type="text"
                   placeholder="Manufacturer"
                   id="medicinemenufacture"
-                  onChange={(e)=>setManufacturer(e.target.value)}
-
+                  onChange={(e) => setManufacturer(e.target.value)}
                 />
               </div>
               <div className="form_flied">
@@ -240,8 +254,7 @@ const handleImageChange = (e) => {
                   type="date"
                   placeholder="Medicine Expair Date"
                   id="ExpairDate"
-                  onChange={(e)=>setExpairDate(e.target.value)}
-
+                  onChange={(e) => setExpairDate(e.target.value)}
                 />
               </div>
               <div className="form_flied">
@@ -250,8 +263,7 @@ const handleImageChange = (e) => {
                   placeholder="Medicine Type"
                   type="text"
                   id="medicinetype"
-                  onChange={(e)=>setVat(e.target.value)}
-
+                  onChange={(e) => setVat(e.target.value)}
                 />
               </div>
               <div className="form_flied">
@@ -278,7 +290,6 @@ const handleImageChange = (e) => {
                       id="active"
                       name="status"
                       value="active"
-
                     />
                   </div>
                   <div
@@ -293,8 +304,7 @@ const handleImageChange = (e) => {
                       id="inactive"
                       name="status"
                       value="inactive"
-                      onChange={(e)=>setStatus(e.target.value)}
-
+                      onChange={(e) => setStatus(e.target.value)}
                     />
                   </div>
                    
@@ -314,20 +324,17 @@ const handleImageChange = (e) => {
                   type="text"
                   id="medicinename"
                   required
-                  onChange={(e)=>setMname(e.target.value)}
-
+                  onChange={(e) => setMname(e.target.value)}
                 />
               </div>
 
               <div className="form_flied">
                 <label htmlFor="generaticname">Generic Name :</label>
                 <input
-                
                   placeholder="Generic Name"
                   type="text"
                   id="generaticname"
-                  onChange={(e)=>setGname(e.target.value)}
-
+                  onChange={(e) => setGname(e.target.value)}
                 />
               </div>
 
@@ -336,12 +343,22 @@ const handleImageChange = (e) => {
                   {" "}
                   Medicine Unit <span style={{ color: "red" }}>*</span> :
                 </label>
-                <select onChange={(e)=>setMunit(e.target.value)} id="medicineunit">
-                <option>Select An Option</option>                  {
-                    units?.map((unit,i)=>{
-                        return <option value={unit.unitname} key={i}>{unit.unitname}</option>
+                <select
+                  onChange={(e) => setMunit(e.target.value)}
+                  id="medicineunit"
+                >
+                  <option>Select An Option</option>{" "}
+                  {units ? (
+                    units.map((unit, i) => {
+                      return (
+                        <option value={unit.unitname} key={i}>
+                          {unit.unitname}
+                        </option>
+                      );
                     })
-                  }
+                  ) : (
+                    <option>Loading......</option>
+                  )}
                 </select>
               </div>
 
@@ -351,8 +368,7 @@ const handleImageChange = (e) => {
                   placeholder="Medicine Details"
                   type="text"
                   id="medicinedetails"
-                  onChange={(e)=>setMedicineDetails(e.target.value)}
-
+                  onChange={(e) => setMedicineDetails(e.target.value)}
                 />
               </div>
 
@@ -366,18 +382,17 @@ const handleImageChange = (e) => {
                   type="text"
                   id="medicineprice"
                   required
-                  onChange={(e)=>setPrice(e.target.value)}
-
+                  onChange={(e) => setPrice(e.target.value)}
                 />
               </div>
 
               <div className="form_flied">
                 <label htmlFor="medicineimage"> Image :</label>
-                <input type="file" id="medicineimage"
-                
-                // onChange={(e)=>setMphoto(e.target.value[0])}
-                onChange={handleImageChange}
-
+                <input
+                  type="file"
+                  id="medicineimage"
+                  // onChange={(e)=>setMphoto(e.target.value[0])}
+                  onChange={handleImageChange}
                 />
               </div>
 
@@ -391,24 +406,31 @@ const handleImageChange = (e) => {
                   type="text"
                   id="manufactureprice"
                   required
-                  onChange={(e)=>setManufacPrice(e.target.value)}
-
+                  onChange={(e) => setManufacPrice(e.target.value)}
                 />
               </div>
               <div className="form_flied">
                 <label htmlFor="medicineigta"> IGTA :</label>
-                <input placeholder="IGTA" type="text" id="medicineigta"
-                      onChange={(e)=>setIGTA(e.target.value)}
-
+                <input
+                  placeholder="IGTA"
+                  type="text"
+                  id="medicineigta"
+                  onChange={(e) => setIGTA(e.target.value)}
                 />
               </div>
 
               <div className="form_flied">
                 <label htmlFor="imgpreview"> Preview :</label>
-                <span style={{width:'70%', textAlign:'left'}} id="imgpreview">
-                <img style={{width:"70px"}} src={mphoto} alt="imapreview" />
+                <span
+                  style={{ width: "70%", textAlign: "left" }}
+                  id="imgpreview"
+                >
+                  <img
+                    style={{ width: "70px" }}
+                    src={mphoto}
+                    alt="imapreview"
+                  />
                 </span>
-                
               </div>
             </div>
           </div>
