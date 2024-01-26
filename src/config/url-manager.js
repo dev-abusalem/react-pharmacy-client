9d30b1228f01c4d06e83cc294b2c3e9590c16be8
dev.abusalem <dev.abusalem@gmail.com>
2024-01-26 23:43:47 +0600

    updatea ldfsafa

--- src/config/url-manager.js
+++ src/config/url-manager.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://react-phermacy-server.vercel.app/";
+    : "https://react-phermacy-server.vercel.app";
